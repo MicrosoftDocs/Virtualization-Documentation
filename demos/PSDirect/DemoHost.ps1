6ad493c01b18f506ff53990899d2c9a7e309b674
Sarah Cooley <scooley@microsoft.com>
2015-10-22 16:45:53 -0700

    Host script done

--- demos/PSDirect/DemoHost.ps1
+++ demos/PSDirect/DemoHost.ps1
@@ -7,14 +7,12 @@
   
  # Check to see if we are currently running "as Administrator"
  if ($myWindowsPrincipal.IsInRole($adminRole))
-    {
+ {
     # We are running "as Administrator" - so change the title and background color to indicate this
     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    $Host.UI.RawUI.BackgroundColor = "DarkGreen"
     clear-host
-    }
- else
-    {
+ } else {
     # We are not running "as Administrator" - so relaunch as administrator
     
     # Create a new process object that starts PowerShell
@@ -33,16 +31,26 @@
     exit
     }
 
-# Run your code that needs to be elevated here
+
+
 
 function demoSetupCleanup{
     Get-VM "psdirect" -Verbose | Get-VMSnapshot -Name "clean" -Verbose | Restore-VMSnapshot -Confirm:$false
     if ((get-vm "psdirect").state -ne "Running") { start-vm "psdirect" }
 }
 
-function guestSetup {
-    while(!(Test-Path "C:\inetpub\wwwroot\HelloWorld\index.html")) { sleep -Milliseconds 500 }
 
+function guest1 {
+    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force ;`
+    while(!(Test-Path "C:\Users\scooley\Desktop\DemoGuest.ps1")) { sleep -Milliseconds 500; } 
+}
+
+function guest2 {
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole, IIS-ManagementScriptingTools
+}
+
+function guest3 {
+    while(!(Test-Path "C:\inetpub\wwwroot\HelloWorld\index.html")) { sleep -Milliseconds 500 }
     new-website -Name "HelloWorld" -PhysicalPath "C:\inetpub\wwwroot\HelloWorld\"
     Stop-Website -Name "Default Web Site"
     Start-Website -Name "HelloWorld"
@@ -79,44 +87,34 @@ Write-Host "Copy-VMFile -Name $vmname -SourcePath $vmscriptpath -DestinationPath
 Copy-VMFile -Name $vmname -SourcePath $vmscriptpath -DestinationPath $guestScriptPath -FileSource Host -Verbose -ErrorAction Inquire
 Write-Host ""
 
-Write-Host "Making sure the file copied and setting the execution policy on the remote host to unrestricted..."
+Write-Host "Make sure the file copied and setting the execution policy on the remote host to unrestricted..."
 Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock { '`'"
 Write-host "    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force; '`'"
-Write-Host '    while(!(Test-Path `"C:\Users\scooley\Desktop\DemoGuest.ps1`")) `'
+Write-Host "    while(!(Test-Path $guestScriptPath)) '`'"
 Write-Host '        { Write-Host -NoNewline "."; sleep -Milliseconds 500 } }'
 
 
-Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock { `
-    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force ;`
-    while(!(Test-Path "C:\Users\scooley\Desktop\DemoGuest.ps1")) { sleep -Milliseconds 500; } }
+Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest1}
 
 write-host ""
-write-host "Press enter to start"
+write-host "Press enter to start setting up IIS"
 read-host
 
-Write-Host "Now we're going to connect to the configured virtual machine using PowerShell Direct."
-Write-Host ""
-
-Write-Host "We're runing the following in $vmname with Invoke-Command:"
-Write-Host 'Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole'
+Write-Host "First, we're using a function to enable the IIS-WebServerRole and IIS-ManagementScriptingTools roles in $vmname"
 Write-Host ""
 Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock `${function:Config}"
 
-Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock { Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole, IIS-ManagementScriptingTools }
+Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest2}
 
 Write-Host ""
-Write-Host "Now let's set the default page to Hello World or something..."
-Write-Host ""
+Write-Host "Next, Let's load a new default page.  Press enter to load the new site."
 Get-ChildItem $sitepath -Recurse -File | % { Copy-VMFile -Name $vmname -SourcePath $_.FullName -DestinationPath ("C:\inetpub\wwwroot\HelloWorld\"+$_) -CreateFullPath -FileSource Host -Force }
 Write-Host ""
+read-host
 
-Write-Host "Making sure the site copied and starting the new site..."
+Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest3}
 
-Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guestSetup}
-
-Write-Host ""
-Write-Host "There you go, unconfigured VM to a running web server."
-write-host
+write-host ""
 Write-Host -NoNewLine "Press any key to continue..."
 Read-Host
 
