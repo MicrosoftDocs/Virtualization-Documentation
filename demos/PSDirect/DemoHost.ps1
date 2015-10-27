72351f4ea616ead5019eebec8c527b30ac62ab5f
Sarah Cooley <scooley@microsoft.com>
2015-10-27 09:43:29 -0700

    simplifying

--- demos/PSDirect/DemoHost.ps1
+++ demos/PSDirect/DemoHost.ps1
@@ -41,15 +41,11 @@ function demoSetupCleanup{
 
 
 function guest1 {
-    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force ;`
-    while(!(Test-Path "C:\Users\scooley\Desktop\DemoGuest.ps1")) { sleep -Milliseconds 500; } 
-}
-
-function guest2 {
+    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force ; `
     Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole, IIS-ManagementScriptingTools
 }
 
-function guest3 {
+function guest2 {
     while(!(Test-Path "C:\inetpub\wwwroot\HelloWorld\index.html")) { sleep -Milliseconds 500 }
     new-website -Name "HelloWorld" -PhysicalPath "C:\inetpub\wwwroot\HelloWorld\"
     Stop-Website -Name "Default Web Site"
@@ -62,8 +58,6 @@ cls
 
 $vmname = "psdirect"
 
-$vmscriptpath = "C:\Users\scooley\scooley-virt-docs\demos\PSDirect\DemoGuest.ps1"
-$guestScriptPath = "C:\Users\scooley\Desktop\DemoGuest.ps1"
 $sitepath = "C:\Users\scooley\scooley-virt-docs\demos\PSDirect\site\"
 
 $username = "Administrator"
@@ -77,37 +71,23 @@ demoSetupCleanup
 
 & vmconnect.exe localhost $vmname
 
-write-host "Press enter to start"
-read-host
-
-Write-Host "I enabled guest file copy so I can copy in a monitoring script.  Then you can watch setup inside the VM."
-Write-Host ""
-
-Write-Host "Copy-VMFile -Name $vmname -SourcePath $vmscriptpath -DestinationPath $guestScriptPath -FileSource Host -Verbose"
-Copy-VMFile -Name $vmname -SourcePath $vmscriptpath -DestinationPath $guestScriptPath -FileSource Host -Verbose -ErrorAction Inquire
-Write-Host ""
-
-Write-Host "Make sure the file copied and setting the execution policy on the remote host to unrestricted..."
-Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock { '`'"
-Write-host "    Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force; '`'"
-Write-Host "    while(!(Test-Path $guestScriptPath)) '`'"
-Write-Host '        { Write-Host -NoNewline "."; sleep -Milliseconds 500 } }'
-
-
-Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest1}
-
-write-host ""
 write-host "Press enter to start setting up IIS"
 read-host
 
-Write-Host "First, we're using a function to enable the IIS-WebServerRole and IIS-ManagementScriptingTools roles in $vmname"
-Write-Host ""
-Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock `${function:Config}"
+Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock { '`'"
+Write-host "    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole, IIS-ManagementScriptingTools }"
 
-Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest2}
+Invoke-Command -VMName $vmname -Credential $adminCred -ScriptBlock ${function:guest1}
 
 Write-Host ""
-Write-Host "Next, Let's load a new default page.  Press enter to load the new site."
+Write-Host "Refresh IE to see default IIS configured in $vmname"
+Write-Host ""
+Write-Host "Next, Let's our own default page."
+Write-Host "Press enter to load the new site."
+
+Write-Host ""
+Write-Host "Invoke-Command -VMName $vmname -Credential `$adminCred -ScriptBlock `${function:SetSite}"
+
 Get-ChildItem $sitepath -Recurse -File | % { Copy-VMFile -Name $vmname -SourcePath $_.FullName -DestinationPath ("C:\inetpub\wwwroot\HelloWorld\"+$_) -CreateFullPath -FileSource Host -Force }
 Write-Host ""
 read-host
