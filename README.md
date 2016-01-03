d81d731dcd600b276e3212b6bd284a94339770ef
brogersyh <brogersyh@yahoo.com>
2016-01-03 14:18:18 -0800

    add dockerfile comments, other misc changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dockerfiles-for-windows
 dockerfiles for SQL Server, Java et al.
+
+TODO: Link to blog post
+
+
--- rm-dangling-images.ps1
+++ rm-dangling-images.ps1
@@ -1,1 +1,3 @@
+# does not work as of Nov-2015 version of docker-windows
+
 docker rmi $(docker images --filter dangling=true)
