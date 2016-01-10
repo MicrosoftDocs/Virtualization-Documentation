96bccd98f49d981ec1a573a8588207327050c3df
brogersyh <brogersyh@yahoo.com>
2016-01-10 14:21:05 -0800

    add comments

--- postgresql/init-config-start-service.ps1
+++ postgresql/init-config-start-service.ps1
@@ -1,3 +1,5 @@
+# postgres init, config and start-service
+
 param(
 [string]$sqldata="c:\sql",
 [string]$pguser
@@ -5,18 +7,14 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+$svcname = "PostgreSQL"
+
 # create database cluster folder
-pg_ctl init -U $pguser
+./pg_ctl init -U $pguser
 
 # reconfigure postgresql for remote access (from default localhost-only)
 add-content (join-path $sqldata "pg_hba.conf") "`nhost all all 0.0.0.0/0 trust"
 add-content (join-path $sqldata "postgresql.conf") "`nlisten_addresses = '*'"
 
-# create and start service
-pg_ctl register
-
-#wait for service to start
-(Get-Service 'PostgreSQL').WaitForStatus('Running', '00:00:30')
-
-# create role (since this is not done by upstream "pg_ctl init")
-createuser -s -r $pguser
+# create service (leaves set to 'Automatic' start)
+./pg_ctl register
--- postgresql/start.ps1
+++ postgresql/start.ps1
@@ -1,10 +1,23 @@
+# create postgres role then do appropriate detached / interactive action
+
 param(
 [ValidateSet('interactive', 'detached', ignorecase=$true)]
 [string]$attachmode="interactive",
-[string]$sqldata="c:\sql"
+[string]$sqldata="c:\sql",
+[string]$pguser="postgres"
 )
 
 $ErrorActionPreference = "Stop"
+
+#create postgres role for postgres user / db if needed (first time this script is run in container)
+./psql -U $pguser -d $pguser -c "\c $pguser" 
+if ($LASTEXITCODE -ne 0) {
+  ./createuser -s -r $pguser
+  if ($LASTEXITCODE -ne 0) {
+    "could not create role"
+    exit 1
+  }
+}
 
 # take interactive / detached action as appropriate
 if ($attachmode -eq "interactive") {
