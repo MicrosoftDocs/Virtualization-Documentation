5b45b84ae130cf68c1081ecc0718b28c41bd80d6
Ender Barillas <enderb@microsoft.com>
2016-05-27 12:06:52 -0700

    Changing script to not work on client
    Can be forced with flag -IgnoreClient

--- windows-server-container-tools/Install-ContainerHost/Install-ContainerHost.ps1
+++ windows-server-container-tools/Install-ContainerHost/Install-ContainerHost.ps1
@@ -72,6 +72,9 @@ param(
 
     [switch]
     $Force,
+    
+    [switch]
+    $IgnoreClient,
 
     [switch]
     $HyperV,
@@ -255,11 +258,21 @@ Install-ContainerHost
 
     if (Test-Client)
     {
-        if (-not $HyperV)
+        
+        if (-not $IgnoreClient)
         {
-            Write-Output "Enabling Hyper-V containers by default for Client SKU"
-            $HyperV = $true
-        }    
+            Write-Warning "We recommend that you use the steps outlined in our documentation at http://aka.ms/containers. \nIf you would like to proceed with this script, include the flag -IgnoreClient."
+            throw "Ran on client without -IgnoreClient flag."
+        }
+        else
+        {
+            if (-not $HyperV)
+            {
+                Write-Output "Enabling Hyper-V containers by default for Client SKU"
+                $HyperV = $true
+            }                
+        }
+        
     }
     #
     # Validate required Windows features
