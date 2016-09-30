a3c7ed3b84ef7e0c7798df3af0a5c40dab3919b8
Patrick Lang <patrick.lang@hotmail.com>
2016-09-30 14:27:12 -0700

    Starting recent error dump

--- windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1
+++ windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1
@@ -1,5 +1,19 @@
-Write-Output "Checking how your system is set up"
+Write-Output "Checking for common problems"
 Invoke-Pester
 
-# TODO: Dump logs to text file
+# TODO: Dump full logs to text file
 # TODO: Highlight errors
+
+Write-Output "Warnings & errors from the last 24 hours"
+$logStartTime = (Get-Date).AddHours(-24)
+
+$logNames = "Microsoft-Windows-Containers-Wcifs/Operational",
+            "Microsoft-Windows-Containers-Wcnfs/Operational",
+            "Microsoft-Windows-Hyper-V-Compute-Admin", 
+            "Microsoft-Windows-Hyper-V-Compute-Operational",
+            "Application"
+$levels = 3,2,1,0
+$providers = "Docker", "Microsoft-Windows-Hyper-V-Compute"
+
+# Get-EventLog -LogName Application -Source Docker -After $logStartTime -EntryType Warning, Error | Format-List TimeGenerated, Message
+Get-WinEvent -FilterHashtable @{Logname=$logNames; StartTime=$logStartTime; Level=$levels; ProviderName=$providers}
