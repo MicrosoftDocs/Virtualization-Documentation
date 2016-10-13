ef3ddd6cb56f5746fe568c992e36508448774727
Sarah Cooley <scooley@microsoft.com>
2016-10-13 10:24:12 -0700

    resolving merge

--- windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1
+++ windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1
@@ -89,7 +89,12 @@ $logNames = "Microsoft-Windows-Containers-Wcifs/Operational",
 $levels = 3,2,1,0
 $providers = "Docker", "Microsoft-Windows-Hyper-V-Compute"
 
+<<<<<<< HEAD
 # TODO: Highlight errors
 Get-WinEvent -FilterHashtable @{Logname=$logNames; StartTime=$logStartTime; Level=$levels; ProviderName=$providers} -ErrorAction Ignore
 
 # TODO: Dump full logs to text file
+=======
+# Get-EventLog -LogName Application -Source Docker -After $logStartTime -EntryType Warning, Error | Format-List TimeGenerated, Message
+Get-WinEvent -FilterHashtable @{Logname=$logNames; StartTime=$logStartTime; Level=$levels; ProviderName=$providers} -ErrorAction Ignore
+>>>>>>> origin/live
