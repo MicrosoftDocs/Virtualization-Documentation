a3285decc64fd9c4b937517ba864904c7de1434e
Sarah Cooley <scooley@microsoft.com>
2017-01-23 14:54:36 -0800

    Adding a script to remove the MemoryReserve registry key (#548)
    * more info + starting files
    * added conditional
    * Fixed things
    * added restart vmms instructions

---
+++ hyperv-tools/root-memory-reserve/clear-MemoryReserve-regkey.ps1
@@ -0,0 +1,49 @@
+
+
+# Get the ID and security principal of the current user account
+ $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+ $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+  
+ # Get the security principal for the Administrator role
+ $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+  
+ # Check to see if we are currently running "as Administrator"
+ if ($myWindowsPrincipal.IsInRole($adminRole))
+ {
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    #$Host.UI.RawUI.BackgroundColor = "DarkGreen"
+    clear-host
+ } else {
+    # We are not running "as Administrator" - so relaunch as administrator
+    
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+    
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+    
+    # Exit from the current, unelevated, process
+    exit
+}
+
+$rootReserveKey = "MemoryReserve"
+$rootReserveKeyPath = "HKLM:Software\Microsoft\Windows NT\CurrentVersion\Virtualization"
+
+Write-Host ""
+Write-Host "Checking $rootReserveKeyPath for $rootReserveKey"
+
+while ($s = Get-ItemProperty $rootReserveKeyPath | Select-Object -ExpandProperty $rootReserveKey -ErrorAction 'silentlycontinue')
+{
+    Write-Host "$rootReserveKey reg key set.  Value: $s MB"
+    Remove-ItemProperty -Path $rootReserveKeyPath -Name $rootReserveKey
+}
+
+Write-Host "Reg key nor present.  Press any key to exit"
+Read-Host
---
+++ hyperv-tools/root-memory-reserve/readme.md
@@ -0,0 +1,13 @@
+# Memory scripts
+
+## clear-MemoryReserve-regkey.ps1
+Clears the (now deprecated) MemoryReserve registry key if it is set.
+
+This key controls how much memory Hyper-V sets aside for the host.  By default, Hyper-V root reserve is:  
+384MB + 30MB per 1GB of physical memory on the host machine.
+
+For changes to take effect, restart the vmms.
+
+``` PowerShell
+Restart-Service vmms
+```
