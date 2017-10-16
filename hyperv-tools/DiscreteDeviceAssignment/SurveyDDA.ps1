82897a219e561f552fc52f32848a121be05195fe
Hadden Hoppert <32854053+haddenh@users.noreply.github.com>
2017-10-16 15:12:18 -0700

    Update SurveyDDA.ps1
    Added functionality to report more errors and MMIO space requirements

--- hyperv-tools/DiscreteDeviceAssignment/SurveyDDA.ps1
+++ hyperv-tools/DiscreteDeviceAssignment/SurveyDDA.ps1
@@ -119,7 +119,7 @@ foreach ($pcidev in $pcidevs) {
     {
         $baseAdd =$mem.Antecedent.SubString($mem.Antecedent.IndexOf("""")+1)
         $baseAdd=$baseAdd.SubString(0,$baseAdd.IndexOf(""""))
-        $mmioRange = gwmi -query "select * from Win32_DeviceMemoryAddress" | Where-Object{$_.StartingAddress -like $baseAdd}
+        $mmioRange = gwmi -query "select * from Win32_DeviceMemoryAddress" | Where-Object{$_.StartingAddress –like $baseAdd}
         $mmioTotal = $mmioTotal + $mmioRange.EndingAddress - $mmioRange.StartingAddress
     }
     if ($mmioTotal -eq 0)
@@ -151,4 +151,3 @@ if ((Get-VMHost).IovSupport -eq $false) {
     Write-Host ""
     (Get-VMHost).IovSupportReasons
 }
-
