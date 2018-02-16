ff102be366e789aa662c75498eb17a4004c55193
Sarah Cooley <scooley@microsoft.com>
2018-02-15 22:25:15 -0800

    comments

--- hyperv-tools/vmgallery-helpers/add-source.ps1
+++ hyperv-tools/vmgallery-helpers/add-source.ps1
@@ -1,3 +1,6 @@
+# Add a new gallery location to Quick Create VM Gallery
+
+# Run add-source [new gallery URI/file path]
 Param(
     [Parameter(Mandatory=$true)]
     [string]$newURI
@@ -17,3 +20,8 @@ Write-Host "Successfully added $newURI to $galleryRegKey"
 Write-Host ""
 Write-Host "New Value is: "
 Write-Host (Get-ItemProperty -Path $galleryRegPath -Name $galleryRegKey).$galleryRegKey
+
+# launch Quick Create to verify!
+
+# If the new gallery location didn't load, check event logs.
+# Event Viewer -> Windows Logs -> Application
