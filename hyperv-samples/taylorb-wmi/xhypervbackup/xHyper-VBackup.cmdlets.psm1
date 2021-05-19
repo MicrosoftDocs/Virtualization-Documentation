53d3332d2228ea12809a2f231c8ec839c7a7c127
Shalin Mehta <shalinmehta@microsoft.com>
2021-05-19 13:37:36 -0700

    added wmiutility methods which hold the implementation for some of the cim functions used in the backup script

--- hyperv-samples/taylorb-wmi/xhypervbackup/xHyper-VBackup.cmdlets.psm1
+++ hyperv-samples/taylorb-wmi/xhypervbackup/xHyper-VBackup.cmdlets.psm1
@@ -2,6 +2,211 @@
 # Updated Author: Shalin Mehta
 # The following functions provide an example for how one could create a backup and a reference point of a VM in Hyper-V
 
+<#
+.SYNOPSIS
+    Helper function that processes a CIMMethodResult/Msvm_ConcreteJob.
+
+.DESCRIPTION
+    Helper function that processes a CIMMethodResult/Msvm_ConcreteJob.
+
+.PARAMETER WmiClass
+    Supplies the WMI class object from where the method is being called.
+
+.PARAMETER MethodName
+    Supplies the method name that the job called.
+
+.INPUTS
+    Input a CIMMethodResult object through the pipeline, or any object with
+    a ReturnValue property and optionally a Job property that is an Msvm_ConcreteJob.
+
+.OUTPUTS
+    Returns the input object on success; throws on error.
+
+.EXAMPLE
+    $job | Trace-CimMethodExecution -WmiClass $VMMS -MethodName ExportSystemDefinition
+        Processes a job for the given class and method, shows progress until it reaches completion.
+#>
+filter Trace-CimMethodExecution {
+    param (
+        [Alias("WmiClass")]
+        [Microsoft.Management.Infrastructure.CimInstance]$CimInstance = $null,
+        [string] $MethodName = $null
+    )
+
+    $errorCode = 0
+    $returnObject = $_
+    $job = $null
+    $shouldProcess = $true
+
+    if ($_.CimSystemProperties.ClassName -eq "Msvm_ConcreteJob") {
+        $job = $_
+    }
+    elseif ((Get-Member -InputObject $_ -name "ReturnValue" -MemberType Properties)) {
+        if (($_.ReturnValue -ne 0) -and ($_.ReturnValue -ne 4096)) {
+            # An error occurred
+            $errorCode = $_.ReturnValue
+            $shouldProcess = $false
+        }
+        elseif ($_.ReturnValue -eq 4096) {
+            if ((Get-Member -InputObject $_ -name "Job" -MemberType Properties) -and $_.Job) {
+                # CIM does not seem to actually populate the non-key fields on a reference, so we need
+                # to go get the actual instance of the job object we got.
+                $job = ($_.Job | Get-CimInstance)
+            }
+            else {
+                throw "ReturnValue of 4096 with no Job object!"
+            }
+        }
+        else {
+            # No job and no error, just exit.
+            return $returnObject
+        }
+    }
+    else {
+        throw "Pipeline input object is not a job or CIM method result!"
+    }
+
+    if ($shouldProcess) {
+        $caption = if ($job.Caption) { $job.Caption } else { "Job in progress (no caption available)" }
+        $jobStatus = if ($job.JobStatus) { $job.JobState } else { "No job status available" }
+        $percentComplete = if ($job.PercentComplete) { $job.PercentComplete } else { 0 }
+        while ($job.JobState -eq 4) {
+            Write-Progress -Activity $caption -Status ("{0} - {1}%" -f $jobStatus, $percentComplete) -PercentComplete $percentComplete
+            Start-Sleep -seconds 1
+            $job = $job | Get-CimInstance
+        }
+
+        if ($job.JobState -ne 7) {
+            if (![string]::IsNullOrEmpty($job.ErrorDescription)) {
+                Throw $job.ErrorDescription
+            }
+            else {
+                $errorCode = $job.ErrorCode
+            }
+        }
+        Write-Progress -Activity $caption -Status $jobStatus -PercentComplete 100 -Completed:$true
+    }
+
+    if ($errorCode -ne 0) {
+        if ($CimInstance -and $MethodName) {
+            $cimClass = Get-CimClass -ClassName $CimInstance.CimSystemProperties.ClassName `
+                -Namespace $CimInstance.CimSystemProperties.Namespace -ComputerName $CimInstance.CimSystemProperties.ServerName
+
+            $methodQualifierValues = ($cimClass.CimClassMethods[$MethodName].Qualifiers["ValueMap"].Value)
+            $indexOfError = [System.Array]::IndexOf($methodQualifierValues, [string]$errorCode)
+            if (($indexOfError -ne "-1") -and $methodQualifierValues) {
+                Throw "ReturnCode: ", $errorCode, " ErrorMessage: '", $cimClass.CimClassMethods[$MethodName].Qualifiers["Values"].Value[$indexOfError], "' - when calling $MethodName"
+            }
+            else {
+                Throw "ReturnCode: ", $errorCode, " ErrorMessage: 'MessageNotFound' - when calling $MethodName"
+            }
+        }
+        else {
+            Throw "ReturnCode: ", $errorCode, "When calling $MethodName - for rich error messages provide classpath and method name."
+        }
+    }
+
+    return $returnObject
+}
+
+<#
+.SYNOPSIS
+    Get the __PATH property from a CIMInstance object.
+
+.DESCRIPTION
+    The Get-CIMInstance cmdlet by default doesn't display the WMI system properties
+    like __SERVER. The properties are available in the CimSystemProperties property
+    except for __PATH. This function will construct the __PATH property and return it.
+
+.EXAMPLE
+    get-ciminstance win32_memorydevice | get-ciminstancepath
+
+    \\SERVER01\root\cimv2:Win32_MemoryDevice.DeviceID="Memory Device 0"
+    \\SERVER01\root\cimv2:Win32_MemoryDevice.DeviceID="Memory Device 1"
+
+.INPUTS
+    A CIMInstance object
+
+.OUTPUTS
+    String representing the path of the input object
+#>
+function Get-CimInstancePath {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline = $True)]
+        [ValidateNotNullorEmpty()]
+        [Microsoft.Management.Infrastructure.CimInstance]$CimInstance
+    )
+
+    $key = $CimInstance.CimClass.CimClassProperties |
+        Where-Object { $_.Qualifiers.Name -contains "key" } |
+        Select-Object -ExpandProperty Name
+
+    $path = ('\\{0}\{1}:{2}{3}' -f $CimInstance.CimSystemProperties.ServerName.ToUpper(),
+        $CimInstance.CimSystemProperties.Namespace.Replace("/", "\"),
+        $CimInstance.CimSystemProperties.ClassName,
+        $(if ($key -is [array]) {
+                # Need a string with every key in the array, keys separated by commas
+                $sep = ""
+                $s = [string]"."
+                foreach ($k in $key) {
+                    $s += "$($sep)$($k)=""$($CimInstance.($k))"""
+                    $sep = ","
+                }
+                $s
+            }
+            elseif ($key) {
+                # just a single key
+                ".$($key)=""$($CimInstance.$key)"""
+            }
+            else {
+                #no key
+                '=@'
+            }).Replace('\', '\\')
+    )
+
+    return $path
+}
+
+<#
+.SYNOPSIS
+    Helper function that converts a CIM instancce to an embedded Unicode string.
+
+.DESCRIPTION
+    Helper function that converts a CIM instancce to an embedded Unicode string.
+    Useful to pass to Hyper-V WMI cmdlets that expect string versions of MSVM
+    classes as parameters.
+
+.PARAMETER CimInstance
+    Supplies the CIM instance object to stringify.
+
+.INPUTS
+    Input a CIM instance object through the pipeline.
+
+.OUTPUTS
+    Returns the Unicode string version of the CIM instance.
+
+.EXAMPLE
+
+
+#>
+function ConvertTo-CimEmbeddedString {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        [Microsoft.Management.Infrastructure.CimInstance]$CimInstance
+    )
+
+    if ($null -eq $CimInstance) {
+        return ""
+    }
+
+    $cimSerializer = [Microsoft.Management.Infrastructure.Serialization.CimSerializer]::Create()
+    $serializedObj = $cimSerializer.Serialize($CimInstance, [Microsoft.Management.Infrastructure.Serialization.InstanceSerializationOptions]::None)
+
+    return [System.Text.Encoding]::Unicode.GetString($serializedObj)
+}
+
 function Convert-VmBackupCheckpoint
 {
     Param(
